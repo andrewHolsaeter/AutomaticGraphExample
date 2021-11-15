7593e0bacd213e0edeef273dbd6f0c137c5e7e48
Andrew <andrew.m.holsaeter@gmail.com>
2021-11-15 12:35:40 +0100

    initial working razor page with msagl

---
+++ WebApplication1/Svg/ISvgElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Msagl.Drawing;
+using System.Xml;
+
+namespace WebApplication1.Svg
+{
+    public interface ISvgElement
+    {
+        Color BackgroundColor { get; set; }
+        Color BorderColor { get; set; }
+        bool Dashed { get; set; }
+        Color FontColor { get; set; }
+        double Height { get; set; }
+        double? StrokeDashArray { get; set; }
+        double StrokeWidth { get; set; }
+        double Width { get; set; }
+        double X { get; set; }
+        double Y { get; set; }
+
+        void WriteTo(XmlWriter writer);
+    }
+}
---
+++ WebApplication1/Svg/SvgRect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Msagl.Drawing;
+using System.Xml;
+
+namespace WebApplication1.Svg
+{
+    public class SvgRect : SvgElement
+    {
+
+        public override void WriteTo(XmlWriter writer)
+        {
+            writer.WriteStartElement("rect");
+            writer.WriteAttribute("x", X);
+            writer.WriteAttribute("y", Y);
+            writer.WriteAttribute("width", Width);
+            writer.WriteAttribute("height", Height);
+            writer.WriteAttribute("stroke", BorderColor);
+            writer.WriteAttribute("fill", BackgroundColor);
+
+            if (this.StrokeDashArray != null)
+            {
+                writer.WriteAttribute("stroke-dasharray", StrokeDashArray);
+            }
+
+            writer.WriteEndElement();
+        }
+
+    }
+}
